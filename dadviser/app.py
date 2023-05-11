1c211968f663b38760f7eb6621492310d26f0546
Alexei Leukhin <Panzerwaffe@users.noreply.github.com>
2023-05-11 12:25:27 +0300

    updated files as on server

--- dadviser/app.py
+++ dadviser/app.py
@@ -1,16 +1,25 @@
 import re
 import chardet
 from core.utils import DAdviser
+import logging
 
-from flask_session import Session
-from flask import Flask, render_template, request, session, redirect, url_for
+# from flask_session import Session
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 
 
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = open('secret').read()
-app.config['SESSION_TYPE'] = 'filesystem'
+# app.config['SESSION_TYPE'] = 'filesystem'
 
-Session(app)
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    datefmt='%d.%m.%Y %I:%M:%S.%p', 
+    filename='out.log', 
+    encoding='utf-8'
+)
+
+
+# Session(app)
 
 rus = re.compile('[^а-яА-ЯёЁ]')
 
@@ -21,7 +30,7 @@ adviser = DAdviser(documents_path)
 @app.errorhandler(Exception)
 def handle_error(e):
     return redirect(url_for('check'))
-
+"""
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -36,24 +45,37 @@ def login():
             return redirect(url_for('check'))
 
     return render_template("login.html")
+"""
 
 @app.route('/', methods=['GET','POST'])
 def check():
-    if not session.get('auth'):
-        return redirect(url_for('login'))
-
+    result = {}
+    logging.info("New request!")
+    
     if request.method == "POST":
-        files = request.files.getlist("files")
-        for file in files:
-            if file.filename.endswith('.txt'):
-                data = file.stream.read()
-                encoding = chardet.detect(data)['encoding']
+        file = request.files.get("files")
+        
+        logging.info("File was upload")    
+
+        if file and file.filename.endswith('.txt'):
+            data = file.stream.read()
+            encoding = chardet.detect(data)['encoding']
+            if encoding:
                 data = rus.sub(' ', data.decode(encoding).lower())
-                result = adviser.get_similarity(data, toplist=10)
+                if len(data.split()) > 20:
+                    logging.info("SUCCESS!")
+                    result = adviser.get_similarity(data, toplist=10)
+                else:
+                    logging.info("WARN: a few russian words...")
+                    flash("В файле мало русских слов (<20)", 'error')
+            else:
+                logging.info("WARN: bad encoding...")
+                flash("Неизвестная кодировка файла", 'error')
+        else:
+            logging.info("WARN: non txt file...")
+            flash("Формат файла не .txt", 'error')
 
-                return render_template('check.html', result=result)
-
-    return render_template('check.html')
+    return render_template('check.html', result=result)
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=5000)
